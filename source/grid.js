4b2099ff6d0a81ed05e0f46492d4d2d915c7a9dc
cedjud <cedricjud@outlook.com>
2017-09-02 21:23:26 +0200

    Create Grid class with columns, rows, column width, row height

---
+++ source/grid.js
@@ -0,0 +1,9 @@
+export default class Grid {
+  constructor( columns = 5, rows = 5, columnWidth = 200, rowHeight = 200){
+    this.columns = columns;
+    this.rows = rows;
+
+    this.columnWidth = columnWidth;
+    this.rowHeight = rowHeight;
+  }
+}

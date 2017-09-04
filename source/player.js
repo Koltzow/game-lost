4ccd0574f42cec68a6aaf1d52d2a950ddc9ce516
Christopher Køltzow <christopherk88@gmail.com>
2017-09-04 22:31:07 +0200

    Make player extend girl

--- source/player.js
+++ source/player.js
@@ -1,21 +1,19 @@
-import { rotatePoint } from './util';
 import Step from './step';
+import Girl from './girl';
 
-export default class Player {
+export default class Player extends Girl {
 
-	constructor() {
+	constructor(x = 0, y = 0) {
 
-		this.x = 0;
-		this.y = 0;
-		this.vx = 0;
-		this.vy = 0;
-		this.radian = 0;
+    super(x, y, '#FF005A');
+
 		this.radius = 12;
 		this.speed = 0.3;
 		this.friction = 0.9;
+		this.sisters = [];
+		this.history = Array(6*20).fill([0,0]);
 		this.colliding = false;
-		this.facecolor = '#FFDABC';
-		this.hoodcolor = '#FF005A';
+
 	}
 
 	testCollision(obj) {
@@ -75,12 +73,18 @@ export default class Player {
 		}
 
 		// apply rotation with lerp
-		this.radian += diff * 0.1;
+		this.radian += diff * 1;
 
 		// update position
 		this.x += this.vx;
 		this.y += this.vy;
 
+		if(Math.abs(Math.sqrt(this.vx*this.vx + this.vy*this.vy)) > 1){
+			this.history.unshift([this.x, this.y]);
+			if(this.history.length > 1000){
+				this.history.pop();
+			}
+		}
 
 		if(game.frame % 10 === 0 && (Math.round(this.vx) !== 0 || Math.round(this.vy) !== 0)){
 
@@ -98,75 +102,28 @@ export default class Player {
 		this.colliding = false;
 
 		// test collision with trees
-		game.trees.every(tree => {
-			if(this.testCollision(tree)){
-				this.resolveCollision(tree);
+		game.sisters.forEach((sister, i) => {
+			if(this.testCollision(sister)){
+				this.sisters.unshift(sister);
+				game.sisters.splice(i, 1);
 			}
+		});
+
+		this.sisters.forEach((sister, i)=> {
+			const dx = (this.history[i*15 + 15][0] - sister.x) * 0.1;
+			const dy = (this.history[i*15 + 15][1] - sister.y) * 0.1;
+			sister.radian = Math.atan2(dy, dx);
+			sister.x += dx;
+			sister.y += dy;
 		});
 	}
 
-	draw(game) {
+  drawBefore(game) {
 
-		// draw shadow
-		game.context.fillStyle = 'rgba(0,0,0,0.2)';
-		game.context.beginPath();
-		game.context.arc( this.x, this.y, this.radius*1.2, 0, 2*Math.PI );
-		game.context.fill();
+    this.sisters.forEach(sister=> {
+			sister.draw(game);
+		});
 
-		// draw basket
-		game.context.fillStyle = '#CC8D56';
-		game.context.beginPath();
-		const p0 = rotatePoint(this.x, this.y, this.radian, this.x, this.y + this.radius);
-		game.context.arc( p0.x, p0.y, this.radius*0.5, 0, 2*Math.PI );
-		game.context.fill();
-
-		// draw circle
-		game.context.fillStyle = this.hoodcolor;
-		game.context.beginPath();
-		game.context.arc( this.x, this.y, this.radius, 0, 2*Math.PI );
-		game.context.fill();
-
-		// draw hood
-		game.context.beginPath();
-		const p1 = rotatePoint(this.x, this.y, this.radian, this.x, this.y - this.radius);
-		game.context.moveTo( p1.x, p1.y );
-		const p2 = rotatePoint(this.x, this.y, this.radian, this.x - this.radius*2.5, this.y + Math.sin(game.frame/15)*5);
-		const b1 = rotatePoint(this.x, this.y, this.radian, this.x - this.radius, this.y - this.radius);
-		const b2 = rotatePoint(this.x, this.y, this.radian, this.x - this.radius, this.y - this.radius*0.5 + Math.cos(game.frame/15)*5);
-		game.context.bezierCurveTo(b1.x, b1.y, b2.x, b2.y, p2.x, p2.y);
-		const p3 = rotatePoint(this.x, this.y, this.radian, this.x, this.y + this.radius);
-		const b3 = rotatePoint(this.x, this.y, this.radian, this.x - this.radius, this.y + this.radius*0.5 + Math.cos(game.frame/15)*5);
-		const b4 = rotatePoint(this.x, this.y, this.radian, this.x - this.radius, this.y + this.radius);
-		game.context.bezierCurveTo(b3.x, b3.y, b4.x, b4.y, p3.x, p3.y);
-		game.context.fill();
-
-		// draw face
-		game.context.fillStyle = this.facecolor;
-		game.context.beginPath();
-		const p4 = rotatePoint(this.x, this.y, this.radian, this.x + this.radius*0.2, this.y);
-		game.context.arc( p4.x, p4.y, this.radius*0.8, this.radian - 1.5, this.radian + 1.5 );
-		game.context.fill();
-
-		// draw nose
-		const nose = rotatePoint(this.x, this.y, this.radian, this.x + this.radius, this.y);
-		game.context.beginPath();
-		game.context.arc( nose.x, nose.y, this.radius * 0.15, 0, 2*Math.PI );
-		game.context.fill();
-
-		// draw left eye
-		game.context.fillStyle = '#000';
-		const left = rotatePoint(this.x, this.y, this.radian, this.x + this.radius * 0.7, this.y - this.radius * 0.3);
-		game.context.beginPath();
-		game.context.arc( left.x, left.y, this.radius * 0.06, 0, 2*Math.PI );
-		game.context.fill();
-
-		// draw right eye
-		const right = rotatePoint(this.x, this.y, this.radian, this.x + this.radius * 0.7, this.y + this.radius * 0.3);
-		game.context.beginPath();
-		game.context.arc(right.x, right.y, this.radius * 0.06, 0, 2*Math.PI );
-		game.context.fill();
-
-
-	}
+  }
 
 }

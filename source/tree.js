850e52af4995239215e4a67c37fe193f34047c2c
Christopher Køltzow <christopherk88@gmail.com>
2017-09-10 17:16:56 +0200

    Refactor tree and add shadows

--- source/tree.js
+++ source/tree.js
@@ -1,15 +1,15 @@
-import { randomBetween } from './util';
+import { randomBetween, rad } from './util';
 
 export default class Tree {
 
-  constructor(x = 0, y = 0, trunkRadius = 20, leafRadius = 100) {
+  constructor({x = 0, y = 0, trunkRadius = 20, leafRadius = 100}) {
 
     this.x = x;
     this.y = y;
-    this.trunkRadius = trunkRadius;
+    this.radius = trunkRadius;
     this.leafRadius = leafRadius;
-    this.amplitude = 0.5;
-    this.detail = 20;
+    this.amplitude = 0;
+    this.detail = 10;
     this.points = Array(this.detail).fill().map((i) => {
       return randomBetween(1-this.amplitude, 1);
     });
@@ -20,20 +20,43 @@ export default class Tree {
 
   draw(game) {
 
+
     // draw trunk
-    game.context.fillStyle = '#111';
+    game.context.fillStyle = 'black';
     game.context.beginPath();
     game.context.arc(
       this.x,
       this.y,
-      this.trunkRadius,
+      this.radius,
       0,
       2*Math.PI
     );
     game.context.fill();
 
+    // draw shadow
+    const rad0 = rad(game.player.x, game.player.y, this.x, this.y);
+    let x1 = this.x + this.radius * Math.cos(rad0 - Math.PI/2 );
+    let y1 = this.y + this.radius * Math.sin(rad0 - Math.PI/2 );
+    let x2 = this.x + this.radius * Math.cos(rad0 + Math.PI/2 );
+    let y2 = this.y + this.radius * Math.sin(rad0 + Math.PI/2 );
+    const rad1 = rad(x2, y2, game.player.x, game.player.y);
+    let x3 = x2 + Math.cos(rad1) * 500;
+    let y3 = y2 + Math.sin(rad1) * 500;
+    const rad2 = rad(x1, y1, game.player.x, game.player.y);
+    let x4 = x1 + Math.cos(rad2) * 500;
+    let y4 = y1 + Math.sin(rad2) * 500;
+
+
+    game.context.fillStyle = 'black';
+    game.context.beginPath();
+    game.context.moveTo(x1, y1);
+    game.context.lineTo(x2, y2);
+    game.context.lineTo(x3, y3);
+    game.context.lineTo(x4, y4);
+    game.context.fill();
+
     // draw top
-    game.context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    game.context.fillStyle = 'rgba(0, 0, 0, 0.1)';
     game.context.beginPath();
     this.points.forEach((p, i) => {
       game.context.lineTo(

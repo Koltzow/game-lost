f69740f1fcc77cf09f4171988ae5ff4db6347b02
Christopher Køltzow <christopherk88@gmail.com>
2017-09-13 13:23:38 +0200

    Fix wolf bug

--- source/wolf.js
+++ source/wolf.js
@@ -6,7 +6,7 @@ export default class Wolf {
 
     this.x = x;
     this.y = y;
-    this.radius = 20;
+    this.radius = 15;
     this.radian = 0;
     this.target = null;
     this.hasEaten = false;
@@ -21,13 +21,20 @@ export default class Wolf {
     }
 
     this.target = null;
+    let lastSister = null;
 
-    if (game.player.sisters.length > 0){
+    game.player.sisters.forEach(sister => {
+      if(!sister.dead){
+        lastSister = sister;
+        return;
+      }
+    });
 
-      const lastSister = game.player.sisters[game.player.sisters.length-1];
+    if (game.player.sisters.length > 0 && lastSister){
 
       if (distanceBetween(this, lastSister) < 0) {
         this.hasEaten = true;
+        lastSister.kill();
       } else if (distanceBetween(this, lastSister) < this.sightDistance) {
         this.target = lastSister;
       }
@@ -36,6 +43,10 @@ export default class Wolf {
 
       if (distanceBetween(this, game.player) < 0) {
         this.hasEaten = true;
+        game.player.kill();
+        game.state = 'FINISHED';
+        return;
+
       } else if (distanceBetween(this, game.player) < this.sightDistance) {
         this.target = game.player;
       }
@@ -57,24 +68,43 @@ export default class Wolf {
 
   draw(game) {
 
-    let color = 'green';
+    let color = '#ccc';
 
     if(this.hasEaten){
-      color = 'red';
+      //color = 'red';
     } else if (this.target) {
-      color = 'yellow';
+      //color = 'yellow';
     }
 
-    // draw body
+    // draw butt
     game.context.fillStyle = color;
+    game.context.beginPath();
+    const p2 = rotatePoint(this.x, this.y, this.radian, this.x - 15, this.y);
+    game.context.arc(p2.x, p2.y, 10, 0, Math.PI*2);
+    game.context.fill();
+
+    // draw body
     game.context.beginPath();
     game.context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
     game.context.fill();
 
     // draw head
     game.context.beginPath();
-    const p0 = rotatePoint(this.x, this.y, this.radian, this.x + 20, this.y);
-    game.context.arc(p0.x, p0.y, 15, 0, Math.PI * 2);
+    const p0 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y);
+    game.context.arc(p0.x, p0.y, 10, 0, Math.PI * 2);
+    game.context.fill();
+
+    // draw snout
+    game.context.beginPath();
+    const p1 = rotatePoint(this.x, this.y, this.radian, this.x + 25, this.y);
+    game.context.arc(p1.x, p1.y, 5, 0, Math.PI * 2);
+    game.context.fill();
+
+    // draw nose
+    game.context.fillStyle = 'black';
+    game.context.beginPath();
+    const p3 = rotatePoint(this.x, this.y, this.radian, this.x + 29, this.y);
+    game.context.arc(p3.x, p3.y, 2, 0, Math.PI * 2);
     game.context.fill();
 
   }

b65cd63d04c9f5797a8b00b09c64446b761d0efa
Christopher Køltzow <christopherk88@gmail.com>
2017-12-21 13:00:29 +0100

    Update menu 2

--- source/menu.js
+++ source/menu.js
@@ -13,17 +13,17 @@ export default class Menu {
     game.context.fillStyle = 'black';
     game.context.fillRect(0, 0, game.width, game.height);
 
-    game.context.fillStyle = 'white';
+    game.context.fillStyle = '#FF005A';
     game.context.textAlign = 'center';
-    game.context.font = '100px Serif';
+    game.context.font = '150px Serif';
     game.context.fillText('Riding Hoods', game.width/2, game.height*0.4);
-    game.context.fillStyle = '#555';
-    game.context.font = '40px Sans-serif';
-    game.context.fillText('Lost in the woods', game.width/2, game.height*0.5);
+    game.context.fillStyle = 'white';
+    game.context.font = '40px Serif';
+    game.context.fillText('Lost in the woods', game.width/2, game.height*0.55);
 
     if (Math.sin(game.frame/15) > 0.2) {
       game.context.fillStyle = 'white';
-      game.context.font = '30px Arial';
+      game.context.font = '30px arial';
       game.context.fillText('Press space to start', game.width/2, game.height*0.8);
     }
 
